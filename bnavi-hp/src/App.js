66e5f58643d7a8327ff8b129935a34a428689a02
goi <kakedashiit@gmail.com>
2019-08-29 00:06:03 +0900

    install material-ui

--- bnavi-hp/src/App.js
+++ bnavi-hp/src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no">
+        </meta>
+          <Button variant="contained" color="secondary">
+            Hello World!
+          </Button>
       </header>
     </div>
   );
